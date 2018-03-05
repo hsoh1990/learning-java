a1af0a43a689bb75d14331358521a15147ef1a28
hsoh <wellstone@hanbat.ac.kr>
2018-03-05 23:27:20 +0900

    FunctionalInterfaceExample03 main class

--- src/main/java/e02/functional/Interface/FunctionalInterfaceExample03.java
+++ src/main/java/e02/functional/Interface/FunctionalInterfaceExample03.java
@@ -2,7 +2,7 @@ package e02.functional.Interface;
 
 public class FunctionalInterfaceExample03 {
     public static void main(String[] args) {
-        System.out.println("FunctionalInterfaceExample03");
+        System.out.println("FunctionalInterfaceExample03..");
 
     }
 }
